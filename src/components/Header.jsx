2c9abf275779fc88e3e04266d4b6eaa49e4fa9f7
Michel Hostettler <michel@hostettler-edv.ch>
2025-12-21 11:34:05 +0100

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1>🚀 Demo WebApp</h1>
+      <p>Eine einfache React-App zum Testen des Dev Self-Service</p>
+    </header>
+  )
+}
